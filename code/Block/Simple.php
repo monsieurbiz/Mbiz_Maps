d8453034d3473cc94b13b15c223bdaeeb64881ef
Léo Peltier <contact@leo-peltier.fr>
2013-11-21 11:11:00 +0100

    Add doc and move code around.

--- code/Block/Simple.php
+++ code/Block/Simple.php
@@ -10,39 +10,92 @@
  */
 class Mbiz_Maps_Block_Simple extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface
 {
-    protected function _toHtml() {
+    /// Render the widget.
+    protected function _toHtml()
+    {
         $cfg = (object) $this->getData();
-        $src = null;
 
-        if($cfg->provider === 'osm') {
-            $src = 'http://www.openstreetmap.org/export/embed.html?';
-            list($lat, $lon) = explode(',', $cfg->coords, 2);
-            $bbox = implode(',', [
-                $lon - 0.001,
-                $lat - 0.001,
-                $lon + 0.001,
-                $lat + 0.001,
-            ]);
-
-            $src .= http_build_query([
-                'bbox'   => $bbox,
-                'marker' => "$lat,$lon",
-            ]);
-        } else {
+        switch($cfg->provider) {
+        case 'osm':
+            return $this->renderOsm($cfg);
+        case 'gmaps':
+            return $this->renderGmaps($cfg);
+        default:
             throw new Exception('Not implemented.');
         }
+    }
 
+
+    /**
+     * Render a Google Maps iframe.
+     *
+     * @param stdClass $cfg data given to the widget.
+     * @return string HTML.
+     */
+    protected function renderGmaps($cfg)
+    {
+        // Google does not provide simple embedding anymore.
+        // Maybe this should be implemented later…
+        throw new Exception('Not implemented.');
+    }
+
+
+    /**
+     * Return a styled iframe for the given URL and size.
+     *
+     * @param string $src URL to embed.
+     * @param int $width
+     * @param int $height
+     */
+    protected function getIframe($src, $width, $height)
+    {
         return "
             <iframe
-                width='{$cfg->width}'
-                height='{$cfg->height}'
-                frameborder='0'
-                scrolling='no'
-                marginheight='0'
-                marginwidth='0'
+                width='$width'
+                height='$height'
                 src='$src'
+                frameborder='0' scrolling='no' marginheight='0' marginwidth='0'
                 style='border: 1px solid black'
             ></iframe>
         ";
+    }
+
+
+    /**
+     * Render an OSM iframe.
+     *
+     * @param stdClass $cfg data given to the widget.
+     * @return string HTML.
+     */
+    protected function renderOsm(stdClass $cfg)
+    {
+        $URL = 'http://www.openstreetmap.org/export/embed.html';
+        list($lat, $lon) = explode(',', $cfg->coords, 2);
+
+        $src = "$URL?" . http_build_query([
+            'bbox'   => $this->getOsmBbox($lat, $lon, .001),
+            'marker' => "$lat,$lon",
+        ]);
+
+        return $this->getIframe($src, $cfg->width, $cfg->height);
+    }
+
+
+    /**
+     * Return a bounding-box for OSM viewport from coordinates plus a zoom factor.
+     * @param float $lat Latitude.
+     * @param float $lon Longitude.
+     * @return string "ay,ax,by,bx" (OSM needs it inverted).
+     */
+    protected function getOsmBbox($lat, $lon, $zoom)
+    {
+        assert('is_float($zoom)');
+
+        return implode(',', [
+            $lon - $zoom,
+            $lat - $zoom,
+            $lon + $zoom,
+            $lat + $zoom,
+        ]);
     }
 }
